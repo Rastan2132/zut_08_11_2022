18e5b37e8f211256198661237952bd2a08800b5c
Rastan2132 <kusvh2132@gmail.com>
2022-11-08 14:35:08 +0100

    Добавьте файлы проекта.

---
+++ zut_08_11_2022/zut_08_11_2022.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	int summ=0, count_min = 0, count = 0, num;
+
+	while (summ < 50 && count_min < 5)
+	{
+		std::cout << "Enter numb\n";
+		std::cin >> num;
+
+		summ = summ +num;
+		if (num < 0)
+			count_min++;
+		count++;
+	}
+	std::cout << "Wprowadzono " << count << " liczb.\n" << "Suma wprowadzonych liczb: " << summ;
+
+	return 0;
+}
